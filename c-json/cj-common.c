6bf7aa2ad91a5fc621f95cf444ed19a015b10bb3
anyway <zorix228337@gmail.com>
2025-11-26 17:46:36 +0300

    now cj(v/o/a)_create functions required a double pointer to object of its types

--- c-json/cj-common.c
+++ c-json/cj-common.c
@@ -11,50 +11,50 @@ CJsonError cjo_realloc(CJsonObject* cjo){
     return CJE_OK;
 }
 
-CJsonError cjo_create(CJsonObject* cjo){
-    cjo = (CJsonObject*)malloc(sizeof(CJsonObject));
-    if(!cjo)
+CJsonError cjo_create(CJsonObject** cjo){
+    *cjo = (CJsonObject*)malloc(sizeof(CJsonObject));
+    if(!*cjo)
         return CJE_ERR_OOM;
 
-    cjo->pairs = (CJsonPair*)calloc(_CJ_DEFAULT_OBJ_CAP_VALUE_, sizeof(CJsonPair));
-    if(!cjo->pairs)
+    (*cjo)->pairs = (CJsonPair*)calloc(_CJ_DEFAULT_OBJ_CAP_VALUE_, sizeof(CJsonPair));
+    if(!(*cjo)->pairs)
             return CJE_ERR_OOM;
 
-    cjo->size = 0;
-    cjo->capacity = _CJ_DEFAULT_OBJ_CAP_VALUE_;
+    (*cjo)->size = 0;
+    (*cjo)->capacity = _CJ_DEFAULT_OBJ_CAP_VALUE_;
 
     return CJE_OK;
 }
 
-CJsonError cjv_create(CJsonValue* cjv){
-    cjv = (CJsonValue*)malloc(sizeof(CJsonValue));
-    if(!cjv)
+CJsonError cjv_create(CJsonValue** cjv){
+    *cjv = (CJsonValue*)malloc(sizeof(CJsonValue));
+    if(!*cjv)
         return CJE_ERR_OOM;
 
-    cjv->value_type = CJ_NULL;
+    (*cjv)->value_type = CJ_NULL;
     
-    cjv->cj_bool_value = false;
-    cjv->cj_number_value = 0;
-    cjv->cj_string_value = NULL;
-    cjv->cj_object_value = NULL;
-    cjv->cj_array_value = NULL;
+    (*cjv)->cj_bool_value = false;
+    (*cjv)->cj_number_value = 0;
+    (*cjv)->cj_string_value = NULL;
+    (*cjv)->cj_object_value = NULL;
+    (*cjv)->cj_array_value = NULL;
 
     return CJE_OK;
 }
 
-CJsonError cja_create(CJsonArray* cja){
-    cja = (CJsonArray*)malloc(sizeof(CJsonArray));
-    if(!cja)
+CJsonError cja_create(CJsonArray** cja){
+    *cja = (CJsonArray*)malloc(sizeof(CJsonArray));
+    if(!*cja)
         return CJE_ERR_OOM;
 
-    cja->size = 0;
-    cja->capacity = _CJ_DEFAULT_ARR_CAP_VALUE_;
+    (*cja)->size = 0;
+    (*cja)->capacity = _CJ_DEFAULT_ARR_CAP_VALUE_;
 
-    cja->data = (CJsonValue*)calloc(_CJ_DEFAULT_ARR_CAP_VALUE_, sizeof(CJsonValue));
-    if(!cja->data)
+    (*cja)->data = (CJsonValue*)calloc(_CJ_DEFAULT_ARR_CAP_VALUE_, sizeof(CJsonValue));
+    if(!(*cja)->data)
         return CJE_ERR_OOM;
 
-    memset(cja->data, 0, sizeof(CJsonValue) * _CJ_DEFAULT_ARR_CAP_VALUE_);
+    memset((*cja)->data, 0, sizeof(CJsonValue) * _CJ_DEFAULT_ARR_CAP_VALUE_);
     
     return CJE_OK;
 }
